4d029bc94c2b7f911f353a94ced8faea5189e841
heruvim <dev1dev2tf@gmail.com>
2014-04-04 21:37:59 +0300

    Добавил код в index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+  include 'data.inc.php';
+  require_once 'lib.inc.php';
+
+
+?>
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    
+    <title>
+     Nazvanie
+    </title>
+    <link href="style.css" rel="stylesheet" />
+    <link href="русинстиль.css" rel="stylesheet" />
+    <script src="vkladki.js"></script>
+			
+			
+		
+		
+		
+   </head>
+   <body>
+		  
+    <header>
+      <?php
+	include 'header.inc.php';
+      ?>
+    </header>
+			
+		  
+			
+    <div id="wrapper">
+
+      <div id="content">
+	<?php
+	  include 'content.inc.php';
+	?>
+      </div>
+		    
+	<footer>
+	 <?php
+	  include 'footer.inc.php';
+	 ?>
+	</footer>
+		  
+      </div>
+		
+  </body>
+</html>
+		
+			
